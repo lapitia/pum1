0ea694b21b664cd4adfe7cfde5e43b60ca79a8d8
lappie <78263704+lapitia@users.noreply.github.com>
2026-03-19 08:31:43 +0100

    Update l2z4.kt

--- lista2/l2z4.kt
+++ lista2/l2z4.kt
@@ -1,11 +1,14 @@
-fun safeParseAndClassify(input: String?): String{
-	if (input.isNullOrBlank()) "brak_danych"
-	return input.toIntOrNull()?.let{
-        number ->
-        if(number%2==0) "parzysta" 
-        else "nieparzysta" ? "brak_danych"
-    }
+fun safeParseAndClassify(input: String?): String {
+    if (input.isNullOrBlank()) return "BRAK_DANYCH" //null lub puste
+
+    return input.toIntOrNull()?.let { number -> //gdy jest int, to idzie dalej, gdy nie, null i później brak danych
+        if (number % 2 == 0) "PARZYSTA" else "NIEPARZYSTA"
+    } ?: "BRAK_DANYCH"
 }
+
 fun main() {
-	
+println(safeParseAndClassify(null)) // BRAK_DANYCH
+println(safeParseAndClassify("")) // BRAK_DANYCH
+println(safeParseAndClassify("10")) // PARZYSTA
+println(safeParseAndClassify("7")) // NIEPARZYSTA
 }
