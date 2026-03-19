587680a6d7aa91dc3a5f7c0f158739f9c27217d8
lappie <78263704+lapitia@users.noreply.github.com>
2026-03-19 16:37:31 +0100

    Update l2z5.kt

--- lista2/l2z5.kt
+++ lista2/l2z5.kt
@@ -1,35 +1,37 @@
 fun check(n: Int, list: List<Int>): Int {
-    if (n <= 0 || list.size <= n) return -1
+    if (n <= 0 || list.size <= n) return -1
 
-    for (i in n until list.size) {
-        val target = list[i] //sprawdzana liczba
-        val preamble = list.subList(i - n, i) //n elementów przed target (od indeksu początkowego włącznie do końcowego wyłącznie)
+    for (i in n until list.size) {
+        val target = list[i]
+        val preamble = list.subList(i - n, i)
 
-        var ok = false //zmienna pomocnicza
+        var ok = false
 
-        for (j in preamble.indices) { //indices - poprawne indeksy
-            for (k in j + 1 until preamble.size) {
-                if (preamble[j] != preamble[k] && preamble[j] + preamble[k] == target) { //wartości mają być różne i dawać aktualną liczbę docelową
-                    ok = true
-                    break
-                }
-            }
-            if (ok) break
-        }
+        for (j in preamble.indices) {
+            for (k in j + 1 until preamble.size) {
+                if (preamble[j] != preamble[k] && preamble[j] + preamble[k] == target) {
+                    ok = true
+                    break
+                }
+            }
+            if (ok) break
+        }
 
-        if (!ok) return target
-    }
+        if (!ok) return target
+    }
 
-    return -1
+    return -1
 }
 
 fun main() {
-    println(check(2, listOf(1, 2, 3, 4, 5, 6))) // 4
+    println(check(2, listOf(1, 2, 3, 4, 5, 6))) 
+    // 4
 
-    println(
-        check(
-            5,
-            listOf(35, 25, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576)
-        )
-    ) // 127
+    println(
+        check(
+            5,
+            listOf(35, 25, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576)
+        )
+    )
+    // 127
 }
